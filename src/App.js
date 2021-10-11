90e1562d28c489fc18918727053180864b477d60
Jose M Carrillo <josemanuelcarrillo.dev@gmail.com>
2021-10-11 07:36:06 -0600

    feat: connect api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import './App.css';
-import { students } from './students';
+import React from 'react';
+// import { students } from './students';
 import List from './components/List'
 import Navbar from './components/Navbar';
 import Layout from './components/Layout'
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <Layout>
-      <Navbar/>
-      <List students={students} />
-      <Footer />
-    </Layout>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      students: {},
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://students.hasura.app/api/rest/students', {
+      method: 'GET',
+      headers: {
+        'x-hasura-admin-secret': '733M3Tgq5IK2ALRXFSivpX86TGJX82goni63azRwZGCtVY1qN4t8521f1LE4iKxq'
+      }
+    }).then(response => response.json())
+      .then(result => {
+        this.setState({
+          students: result.students,
+          isLoaded: true
+      })
+    })
+  }
+
+  render(){
+    const { students, isLoaded } = this.state
+    return (
+      <Layout>
+        <Navbar/>
+        {!isLoaded ? <p>loading...</p> : <List students={students}/>}
+        <Footer />
+      </Layout>
+    );
+  }
 }
 
 export default App;
